9e9f4f652b37ebdd1d06e4822de17661737a8f55
marcos-alcantara <marcos-alcantara@hotmail.com>
2019-03-11 10:51:18 -0600

    getPersonaCIB

--- application/controllers/Solicitud.php
+++ application/controllers/Solicitud.php
@@ -1231,5 +1231,30 @@
 			echo json_encode( [ 'results' => $responseModel ] );
 			exit;
 		}
-
-    }
+		/*
+		* XMAL obtener datos pestaña "Laboral" -> "Comisiones"
+		* get getPersonaCIB pID_ALTERNA,pCURP
+		# EJEMPLO: http://localhost/SGP/Solicitud/getPersonaCIB?pID_ALTERNA=56
+		*/
+		public function getPersonaCIB(){
+			if (! $this->input->is_ajax_request()) {
+				if (ENVIRONMENT == 'production') redirect('Error/e404','location');
+			}
+			$idAlterna = $this->input->get('pID_ALTERNA');
+			$curp = $this->input->get('pCURP');
+			try {
+				$this->load->model('SOLICITUD_model');
+				$responseModel = $this->SOLICITUD_model->sp_B1_getPersonaCIB($idAlterna,$curp);
+			} 
+			catch (rulesException $e){	
+				header("HTTP/1.0 400 " . utf8_decode($e->getMessage()));
+			}
+			catch (Exception $e) {
+				header("HTTP/1.0 500 Internal Server Error");
+			}
+			
+			header('Content-type: application/json');
+			echo json_encode( [ 'results' => $responseModel ] );
+			exit;
+		}
+}
