983e75290b32cc586b603d33542ab2f09edb7121
marcos-alcantara <marcos-alcantara@hotmail.com>
2019-03-14 16:38:11 -0600

    laboral

--- application/controllers/Solicitud.php
+++ application/controllers/Solicitud.php
@@ -1367,6 +1367,55 @@
 			exit;
 		}
 
+		# XMAL obtener datos pestaña "Laboral" -> "Adscripción de la persona"
+		# get vwAdscripcion pID_ALTERNA,pCURP
+		# EJEMPLO: http://localhost/SGP/Solicitud/vwAdscripcion?pID_ALTERNA=56
+		public function vwAdscripcion(){
+			if (! $this->input->is_ajax_request()) {
+				if (ENVIRONMENT == 'production') redirect('Error/e404','location');
+			}
+			$idAlterna = $this->input->get('pID_ALTERNA');
+			$curp = $this->input->get('pCURP');
+			try {
+				$this->load->model('SOLICITUD_model');
+				$responseModel = $this->SOLICITUD_model->sp_B1_vwAdscripcion($idAlterna,$curp);
+			} 
+			catch (rulesException $e){	
+				header("HTTP/1.0 400 " . utf8_decode($e->getMessage()));
+			}
+			catch (Exception $e) {
+				header("HTTP/1.0 500 Internal Server Error");
+			}
+			
+			header('Content-type: application/json');
+			echo json_encode( [ 'results' => $responseModel ] );
+			exit;
+		}
+		# XMAL obtener datos pestaña "Laboral" -> "Empleos diversos"
+		# get vwEmpleoAdicional pID_ALTERNA,pCURP
+		# EJEMPLO: http://localhost/SGP/Solicitud/vwEmpleoAdicional?pID_ALTERNA=56
+		public function vwEmpleoAdicional(){
+			if (! $this->input->is_ajax_request()) {
+				if (ENVIRONMENT == 'production') redirect('Error/e404','location');
+			}
+			$idAlterna = $this->input->get('pID_ALTERNA');
+			$curp = $this->input->get('pCURP');
+			try {
+				$this->load->model('SOLICITUD_model');
+				$responseModel = $this->SOLICITUD_model->sp_B2_LAB_vwEmpleoAdicional($idAlterna,$curp);
+			} 
+			catch (rulesException $e){	
+				header("HTTP/1.0 400 " . utf8_decode($e->getMessage()));
+			}
+			catch (Exception $e) {
+				header("HTTP/1.0 500 Internal Server Error");
+			}
+			
+			header('Content-type: application/json');
+			echo json_encode( [ 'results' => $responseModel ] );
+			exit;
+		}
+
 # [OBTENER DATOS]
 
 # Datos generales
--- application/models/SOLICITUD_model.php
+++ application/models/SOLICITUD_model.php
@@ -690,6 +690,29 @@ class SOLICITUD_model extends MY_Model
     }
     return $this->response;
   }
+  public function sp_B1_vwAdscripcion($idAlterna = null,$curp = null){
+    $this->procedure('sp_B1_vwAdscripcion');
+    $this->addParam('pCURP',$curp,'N');
+    $this->addParam('pID_ALTERNA',$idAlterna);
+
+    $buid = $this->build_query();
+    $query = $this->db->query($buid);
+    $response = $this->query_row($query);
+
+    if($response === FALSE){
+      $this->response['status'] = 0;
+      $this->response['message'] = 'Ha ocurrido un error al procesar su última acción.';
+    }else{
+      if(count($response) > 0){
+        $this->response['status'] = 1;
+        $this->response['data'] = $this->try_result($response);
+      }else{
+        $this->response['status'] = 0;
+        $this->response['message'] = 'No se encontraron resultados.';
+      }
+    }
+    return $this->response;
+  }
   /*
   * "Opcion Nueva Solicitud - Ficha Laboral - Pestaña Adscripción Actual
   * Boton Guardar Adscipcion.
@@ -828,6 +851,29 @@ class SOLICITUD_model extends MY_Model
     $buid = $this->build_query();
     $query = $this->db->query($buid);
     $response = $this->query_list($query);
+
+    if($response === FALSE){
+      $this->response['status'] = 0;
+      $this->response['message'] = 'Ha ocurrido un error al procesar su última acción.';
+    }else{
+      if(count($response) > 0){
+        $this->response['status'] = 1;
+        $this->response['data'] = $this->try_result($response);
+      }else{
+        $this->response['status'] = 0;
+        $this->response['message'] = 'No se encontraron resultados.';
+      }
+    }
+    return $this->response;
+  }
+  public function sp_B2_LAB_vwEmpleoAdicional($idAlterna = null,$curp = null){
+    $this->procedure('sp_B2_LAB_vwEmpleoAdicional');
+    $this->addParam('pCURP',$curp,'N');
+    $this->addParam('pID_ALTERNA',$idAlterna);
+
+    $buid = $this->build_query();
+    $query = $this->db->query($buid);
+    $response = $this->query_row($query);
 
     if($response === FALSE){
       $this->response['status'] = 0;
