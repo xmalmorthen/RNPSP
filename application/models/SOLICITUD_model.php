e4e36b66453b0f9aa31411d2b71117e51c05b703
marcos-alcantara <marcos-alcantara@hotmail.com>
2019-03-15 00:55:51 -0600

    EJEMPLO: http://localhost/SGP/Solicitud/vwSenasParticulares?pID_ALTERNA=56

--- application/models/SOLICITUD_model.php
+++ application/models/SOLICITUD_model.php
@@ -1600,6 +1600,31 @@ class SOLICITUD_model extends MY_Model
     return $this->response;
   }
 
+  # Opcion VER - Ficha Identificación- Pestaña Señas Particulares
+  # sp_B2_MF_vwSenasParticulares - Muestra la ultima seña particular correspondientes al elemento policial.
+  public function sp_B2_MF_vwSenasParticulares($idAlterna = null,$curp = null){
+    $this->procedure('sp_B2_MF_vwSenasParticulares');
+    $this->addParam('pCURP',$curp,'N');
+    $this->addParam('pID_ALTERNA',$idAlterna);
+
+    $buid = $this->build_query();
+    $query = $this->db->query($buid);
+    $response = $this->query_row($query);
+
+    if($response === FALSE){
+      $this->response['status'] = 0;
+      $this->response['message'] = 'Ha ocurrido un error al procesar su última acción.';
+    }else{
+      if(count($response) > 0){
+        $this->response['status'] = 1;
+        $this->response['data'] = $this->try_result($response);
+      }else{
+        $this->response['status'] = 0;
+        $this->response['message'] = 'No se encontraron resultados.';
+      }
+    }
+    return $this->response;
+  }
   
 
   # ****************************************************************************************************************
